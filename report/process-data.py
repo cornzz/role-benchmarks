7292fd51cd452e3ef6bcfdaab33652bba0c174e2
cornzz <39997278+cornzz@users.noreply.github.com>
2021-02-28 21:16:35 +0100

    Update process_data.py to also generate non normalized charts

--- report/process-data.py
+++ report/process-data.py
@@ -12,16 +12,18 @@ def load_benchmark_data(path: str) -> List[pd.DataFrame]:
     col_drop = ['Invocation', 'Iteration', 'Unit', 'Criterion', 'VM', 'Extra', 'Cores', 'InputSize']
     data.drop(col_drop, axis=1, inplace=True)
     data['Var'] = data['Var'].map(lambda x: round((x / 1000)**2, 2))
+    data = data.set_index(['Var', 'Approach']).sort_index()
     return [x.drop('Benchmark', axis=1) for _, x in data.groupby('Benchmark')]
 
 
 def separate_baseline(data: pd.DataFrame, mask: str) -> List[pd.DataFrame]:
-    return data[data['Approach'] != mask], data[data['Approach'] == mask]
+    bl = data.xs(mask, level=1, drop_level=False)
+    return data.drop(bl.index), bl
 
 
 def normalize_data(data: pd.DataFrame, baseline: pd.DataFrame) -> pd.DataFrame:
     bl_mean = baseline.groupby('Var')['Value'].mean()
-    data_norm = data.copy().set_index(['Var', 'Approach']).sort_index()
+    data_norm = data.copy()
     data_norm['Value'] = data_norm['Value'] / bl_mean
     return data_norm
 
@@ -31,28 +33,56 @@ def mean_and_std(data: pd.DataFrame):
     return data.mean(), data.std()
 
 
+def configure_plt_style():
+    plt.style.use('ggplot')
+
+
 def plot_data(labels, means, errors, appr, title):
     x = np.arange(len(labels))
     h = [x.to_numpy().flatten() for x in means]
-    w = 0.4
+    w = 0.35
     yerr = [x.to_numpy().flatten() for x in errors]
     cs = 2.5
     fig, ax = plt.subplots()
 
-    ax.bar(x=(x - w/2), height=h[0], width=w, yerr=yerr, label=appr[0], capsize=cs)
-    ax.bar(x=(x + w/2), height=h[1], width=w, yerr=yerr, label=appr[1], capsize=cs)
+    if len(appr) == 2:
+        ax.bar(x=(x - w/2), height=h[0], width=w, yerr=yerr[0], label=appr[0], capsize=cs)
+        ax.bar(x=(x + w/2), height=h[1], width=w, yerr=yerr[1], label=appr[1], capsize=cs)
+        ax.set_ylabel('Run time factor normalized to Classic 2020')
+    else:
+        ax.bar(x=(x - w), height=h[0], width=w, yerr=yerr[0], label=appr[0], capsize=cs)
+        ax.bar(x=x,       height=h[1], width=w, yerr=yerr[1], label=appr[1], capsize=cs)
+        ax.bar(x=(x + w), height=h[2], width=w, yerr=yerr[2], label=appr[2], capsize=cs)
+        ax.set_ylabel('Run time in ms')
 
-    ax.set_ylabel('Runtime factor normalized to Classic 2020')
     ax.set_xticks(x)
     ax.set_xticklabels(labels)
     ax.legend()
     ax.set_title(title)
-    # fig.tight_layout()
+    fig.tight_layout()
     plt.savefig(f'{title}.svg')
     plt.show()
 
 
 df1, df2 = load_benchmark_data('../data/001-2ab4f810/benchmark.data')
+# Calculate mean and std
+df1_mean, df1_std = mean_and_std(df1)
+df2_mean, df2_std = mean_and_std(df2)
+# Plot data
+configure_plt_style()
+iter_vars = df1.index.levels[0]
+approaches = df1.index.levels[1]
+plot_data(iter_vars,
+          [df1_mean.xs(x, level=1) for x in approaches],
+          [df1_std.xs(x, level=1) for x in approaches],
+          approaches,
+          'Static Context Benchmark_')
+plot_data(iter_vars,
+          [df2_mean.xs(x, level=1) for x in approaches],
+          [df2_std.xs(x, level=1) for x in approaches],
+          approaches,
+          'Variable Contexts Benchmark_')
+
 # Separate baseline
 bl_mask = 'test-objectteams-classic-38'
 df1, df1_bl = separate_baseline(df1, bl_mask)
@@ -61,17 +91,17 @@ df2, df2_bl = separate_baseline(df2, bl_mask)
 df1_norm = normalize_data(df1, df1_bl)
 df2_norm = normalize_data(df2, df2_bl)
 # Calculate mean and std
-df1_mean, df1_std = mean_and_std(df1_norm)
-df2_mean, df2_std = mean_and_std(df2_norm)
-# Plot data
-approaches = df1_mean.index.levels[1]
-plot_data(df1_mean.index.levels[0],
-          [df1_mean.xs(x, level=1) for x in approaches],
-          [df1_std.xs(x, level=1) for x in approaches],
+df1_norm_mean, df1_norm_std = mean_and_std(df1_norm)
+df2_norm_mean, df2_norm_std = mean_and_std(df2_norm)
+# Plot normalized data
+approaches = approaches.drop(bl_mask)
+plot_data(iter_vars,
+          [df1_norm_mean.xs(x, level=1) for x in approaches],
+          [df1_norm_std.xs(x, level=1) for x in approaches],
           approaches,
           'Static Context Benchmark')
-plot_data(df2_mean.index.levels[0],
-          [df2_mean.xs(x, level=1) for x in approaches],
-          [df2_std.xs(x, level=1) for x in approaches],
+plot_data(iter_vars,
+          [df2_norm_mean.xs(x, level=1) for x in approaches],
+          [df2_norm_std.xs(x, level=1) for x in approaches],
           approaches,
           'Variable Contexts Benchmark')
