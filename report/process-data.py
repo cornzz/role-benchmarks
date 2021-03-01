c15c04c5bda033720138d87149acbe28d1afa9d1
cornzz <39997278+cornzz@users.noreply.github.com>
2021-03-01 16:48:38 +0100

    Improve plot look and feel
    Save figures as pdf

--- report/process-data.py
+++ report/process-data.py
@@ -1,8 +1,10 @@
+import sys
 from typing import List
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+# import seaborn as sns
 
 
 def load_benchmark_data(path: str) -> List[pd.DataFrame]:
@@ -30,41 +32,55 @@ def normalize_data(data: pd.DataFrame, baseline: pd.DataFrame) -> pd.DataFrame:
 
 def mean_and_std(data: pd.DataFrame):
     data = data.copy().groupby(['Var', 'Approach'])
-    return data.mean(), data.std()
+    return data.mean(), data.std(ddof=0)
 
 
 def configure_plt_style():
     plt.style.use('ggplot')
+    # sns.set(style='ticks', palette='Set2')
 
 
 def plot_data(labels, means, errors, appr, title):
-    x = np.arange(len(labels))
+    appr = appr.map({'test-objectteams-classic-38': 'Classic 2020',
+                     'test-objectteams-indy-38': 'Polymorphic Dispatch Plans',
+                     'test-objectteams-indy-38-deg': 'PDP w/ Degradation'})
+    label_fs, tick_label_fs = 10, 8
+    x = np.arange(len(labels)) * 1.5
     h = [x.to_numpy().flatten() for x in means]
-    w = 0.35
+    w = 0.4
     yerr = [x.to_numpy().flatten() for x in errors]
     cs = 2.5
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(7, 5))
 
     if len(appr) == 2:
         ax.bar(x=(x - w/2), height=h[0], width=w, yerr=yerr[0], label=appr[0], capsize=cs)
         ax.bar(x=(x + w/2), height=h[1], width=w, yerr=yerr[1], label=appr[1], capsize=cs)
-        ax.set_ylabel('Run time factor normalized to Classic 2020')
+        ax.set_ylabel('Run time factor normalized to Classic 2020', fontsize=label_fs)
     else:
-        ax.bar(x=(x - w), height=h[0], width=w, yerr=yerr[0], label=appr[0], capsize=cs)
-        ax.bar(x=x,       height=h[1], width=w, yerr=yerr[1], label=appr[1], capsize=cs)
-        ax.bar(x=(x + w), height=h[2], width=w, yerr=yerr[2], label=appr[2], capsize=cs)
-        ax.set_ylabel('Run time in ms')
+        ax.bar(x=(x - w), height=h[0], width=w, yerr=yerr[0], label=appr[0], capsize=cs, log=True)
+        ax.bar(x=x,       height=h[1], width=w, yerr=yerr[1], label=appr[1], capsize=cs, log=True)
+        ax.bar(x=(x + w), height=h[2], width=w, yerr=yerr[2], label=appr[2], capsize=cs, log=True)
+        ax.set_ylabel('Run time in ms', fontsize=label_fs)
 
+    ax.set_xlabel('Millions of iterations', fontsize=label_fs)
     ax.set_xticks(x)
     ax.set_xticklabels(labels)
-    ax.legend()
-    ax.set_title(title)
+    ax.tick_params(labelsize=tick_label_fs)
+    ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=len(appr), mode="expand", borderaxespad=0.,
+              fontsize='small')
+    ax.set_title(title, pad=30)
     fig.tight_layout()
-    plt.savefig(f'{title}.svg')
+    plt.savefig(f'{folder}/{title}.pdf')
     plt.show()
 
 
-df1, df2 = load_benchmark_data('../data/001-2ab4f810/benchmark.data')
+try:
+    folder = f'../data/{sys.argv[1]}'
+except IndexError:
+    with open('../data/latest') as f:
+        folder = f'../data/{f.read().split("/")[-1]}'
+
+df1, df2 = load_benchmark_data(f'{folder}/benchmark.data')
 # Calculate mean and std
 df1_mean, df1_std = mean_and_std(df1)
 df2_mean, df2_std = mean_and_std(df2)
