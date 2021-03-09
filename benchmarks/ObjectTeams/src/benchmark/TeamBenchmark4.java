b5301279f1cc17341e083c0cb06759c125c3b646
cornzz <39997278+cornzz@users.noreply.github.com>
2021-03-09 15:32:37 +0100

    Add more contexts to TeamBenchmark4

--- benchmarks/ObjectTeams/src/benchmark/TeamBenchmark4.java
+++ benchmarks/ObjectTeams/src/benchmark/TeamBenchmark4.java
@@ -4,16 +4,17 @@ import benchmark.team.Base;
 import benchmark.team.Team1;
 import benchmark.team.Team2;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 
 public class TeamBenchmark4 extends Benchmark {
 
-	static Logger logger = LoggerFactory.getLogger(TeamBenchmark4.class);
+	// static Logger logger = LoggerFactory.getLogger(TeamBenchmark4.class);
 
-	private Team1 team1;
-	private Team2 team2;
-	private Team1 team3;
+	private Team1 team11;
+	private Team1 team12;
+	private Team2 team21;
+	private Team2 team22;
 	private Base a;
 	private Base b;
 
@@ -22,61 +23,74 @@ public class TeamBenchmark4 extends Benchmark {
 		float n = 100.0f;
 		int iterations = innerIterations * innerIterations;
 		for (int i = 0; i < iterations; i++) {
-			// logger.info("-------- Context 1: Team1 --------");
-			team1.activate();
+			// logger.info("-------- Context 1: Team1 --------");						// TeamBenchmark4: 1, Team1: 1, Team2: 0
+			team11.activate();
 			a.operation1(n);
-			// logger.info("100 -- {}", m);
 			b.operation2(n);
-			// logger.info("100 -- {}", m);
 
-			// logger.info("-------- Context 2: Team2, Team1 --------");
-			team2.activate();
+			// logger.info("-------- Context 2: Team2, Team1 --------");				// TeamBenchmark4: 2, Team1: 1, Team2: 1
+			team21.activate();
 			a.operation1(n);
-			// logger.info("200 -- {}", m);
 			b.operation2(n);
-			// logger.info("50 -- {}", m);
 
-			// logger.info("-------- Context 3: Team2 --------");
-			team1.deactivate();
+			// logger.info("-------- Context 3: Team2, Team2, Team1 --------");		// TeamBenchmark4: 3, Team1: 1, Team2: 2
+			team22.activate();
 			a.operation1(n);
-			// logger.info("400 -- {}", m);
 			b.operation2(n);
-			// logger.info("25 -- {}", m);
 
-			// logger.info("-------- Context 4: Team1, Team2 --------");
-			team1.activate();
+			// logger.info("-------- Context 4: Team2, Team2 --------");				// TeamBenchmark4: 4, Team1: 1, Team2: 4
+			team11.deactivate();
 			a.operation1(n);
-			// logger.info("200 -- {}", m);
 			b.operation2(n);
-			// logger.info("50 -- {}", m);
 
-			// logger.info("-------- Context 5: No active teams --------");
+			// logger.info("-------- Context 5: Team2 --------");						// TeamBenchmark4: *5*, Team1: 1, Team2: 4
 			// Unstable in TeamBenchmarks4
-			team1.deactivate();
-			team2.deactivate();
+			team21.deactivate();
 			a.operation1(n);
-			// logger.info("200 -- {}", m);
 			b.operation2(n);
-			// logger.info("50 -- {}", m);
 
-			// logger.info("-------- Context 6: Team1, Team1 --------");
-			team1.activate();
-			team3.activate();
+			// logger.info("-------- Context 6: Team1, Team2 --------");				// TeamBenchmark4: *6*, Team1: 2, Team2: 4
+			team11.activate();
 			a.operation1(n);
-			// logger.info("50 -- {}", m);
 			b.operation2(n);
-			// logger.info("200 -- {}", m);
-			team3.deactivate();
-			team1.deactivate();
+
+			// logger.info("-------- Context 7: Team1, Team1, Team2 --------");		// TeamBenchmark4: *7*, Team1: 3, Team2: 4
+			team12.activate();
+			a.operation1(n);
+			b.operation2(n);
+
+			// logger.info("-------- Context 8: Team1, Team1 --------");				// TeamBenchmark4: *8*, Team1: 4, Team2: 4
+			team22.deactivate();
+			a.operation1(n);
+			b.operation2(n);
+
+			// logger.info("-------- Context 9: Team2, Team1, Team1 --------");		// TeamBenchmark4: *9*, Team1: 4, Team2: *5*
+			team21.activate();
+			a.operation1(n);
+			b.operation2(n);
+
+			// logger.info("-------- Context 10: Team2, Team2, Team1, Team1 --------");	// TeamBenchmark4: *10*, Team1: 4, Team2: *6*
+			team22.activate();
+			a.operation1(n);
+			b.operation2(n);
+
+			// logger.info("-------- Context 11: No active teams --------");			// TeamBenchmark4: *11*, Team1: 4, Team2: *6*
+			team22.deactivate();
+			team21.deactivate();
+			team12.deactivate();
+			team11.deactivate();
+			a.operation1(n);
+			b.operation2(n);
 		}
 
 		return true;
 	}
 
 	public boolean setUp(final int innerIterations) {
-        team1 = new Team1();
-        team2 = new Team2();
-        team3 = new Team1();
+        team11 = new Team1();
+        team12 = new Team1();
+        team21 = new Team2();
+        team22 = new Team2();
         a = new Base(1);
         b = new Base(2);
 
